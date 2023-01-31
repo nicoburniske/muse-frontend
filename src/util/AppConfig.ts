860f7076b2555e4d0a72bd06946c4dd64807ee08
Nico Burniske <nicoburniske@gmail.com>
2023-01-30 21:28:18 -0500

    add vite prefix to environment variable

--- src/util/AppConfig.ts
+++ src/util/AppConfig.ts
@@ -1,8 +1,9 @@
 export const AppConfig = (() => {
+    // Need to prefix all environment variables with VITE_
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const MODE = import.meta.env.MODE
     const DEV = import.meta.env.DEV
-    const PROD = import.meta.env.DEV
+    const PROD = import.meta.env.PROD
 
     return {
         MODE,
