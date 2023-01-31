026ab5e2a2f3e201463fa3848d7c028d2a01b6b5
Nico Burniske <nicoburniske@gmail.com>
2023-01-31 13:40:43 -0500

    renamed atom parameter

--- src/platform/atomValueOrSuspend.ts
+++ src/platform/atomValueOrSuspend.ts
@@ -1,7 +1,7 @@
 import { Atom, atom } from 'jotai'
 
-const atomValueOrSuspend = <T,>(value: Atom<T | null | undefined>) => atom<Promise<T>>((get) => {
-    const currentValue = get(value)
+const atomValueOrSuspend = <T,>(valueAtom: Atom<T | null | undefined>) => atom<Promise<T>>((get) => {
+    const currentValue = get(valueAtom)
     if (currentValue) {
         return Promise.resolve(currentValue)
     }
